0bb1af75e4486cc93af90c56253de4d69dfa2df2
YeonjunKo <yeonjunko@gmail.com>
2026-02-05 12:04:08 -0800

    mob chasing the player implemented

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,7 +1,6 @@
 extends Node
 
 @export var mob_scene: PackedScene
-var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,11 +13,9 @@ func _process(delta: float) -> void:
 
 
 func game_over() -> void:
-	$ScoreTimer.stop()
 	$MobTimer.stop()
 
 func new_game():
-	score = 0
 	$Commander.start($StartPosition.position)
 	$StartTimer.start()
 
@@ -32,16 +29,9 @@ func _on_mob_timer_timeout() -> void:
 	mob_spawn_location.progress_ratio = randf()
 	
 	# Set the mob's position
-	mob.position = mob_spawn_location.global_position
+	mob.position = mob_spawn_location.position + Vector2(100, 0)
 	
-	# Direction toward the Player
-	var direction = ($Commander.global_position - mob.position).normalized()
-	
-	# Set rotation so the mob faces the player
-	mob.rotation = direction.angle()
-	# Set velocity
-	var velocity = randf_range(150.0, 250.0)
-	mob.linear_velocity = direction * velocity
+	mob.target = $Commander
 	
 	# Spawn the mob
 	add_child(mob)
@@ -49,8 +39,3 @@ func _on_mob_timer_timeout() -> void:
 
 func _on_start_timer_timeout() -> void:
 	$MobTimer.start()
-	$ScoreTimer.start()
-
-
-func _on_score_timer_timeout() -> void:
-	score += 1
--- scenes/mob.gd
+++ scenes/mob.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 
+var target: Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,5 +8,15 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	if target == null:
+		return
+
+	# Direction toward the Player
+	var direction = (target.global_position - global_position).normalized()
+		
+	# Set rotation so the mob faces the player
+	rotation = direction.angle()
+	# Set velocity
+	var velocity = randf_range(150.0, 250.0)
+	linear_velocity = direction * velocity
