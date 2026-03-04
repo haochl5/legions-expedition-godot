f1247d74ce05980d414bdee347339bf4bcaa2824
Zongze Wu <astrowu61@gmail.com>
2026-03-04 15:39:15 -0800

    trying URL way to identify the urser's group

--- GameManager.gd
+++ GameManager.gd
@@ -3,19 +3,29 @@ extends Node
 var player_group: String = "unknown"
 
 func _ready():
+	var detected_group = "unknown"
+	
 	if OS.has_feature("web"):
-		var js_group = JavaScriptBridge.eval("localStorage.getItem('ab-group');")
-		if js_group != null:
-			player_group = js_group
-		else:
-			player_group = "A"
-			print("This group A, is assigned by default cause no ab-group in localstorage")
+		print("trying URL way to get our group")
+		var current_url = JavaScriptBridge.eval("window.location.href")
+		if "version-b" in current_url or "version_b" in current_url:
+			detected_group = "B"
+		elif "version-a" in current_url or "version_a" in current_url:
+			detected_group = "A"
+		
+		if detected_group == "unknown":
+			print("no idea which URL, try localStorage")
+			var js_group = JavaScriptBridge.eval("localStorage.getItem('ab-group');")
+			if js_group != null:
+				detected_group = js_group
+			else:
+				detected_group = "A" 
+				print("Group is A, because no idea which URL, and no idea what hack is localStorage")
 	else:
-		player_group = "A"
-		print("This group A, is assigned by default cause no ab-group in localstorage, and you are not in web")
-	
-	print("Current Player Group: ", player_group)
-	
+		detected_group = "A" 
+
+	player_group = detected_group
+	print("Final Resolved Group: ", player_group)
 	_sync_to_talo()
 
 func _sync_to_talo():
